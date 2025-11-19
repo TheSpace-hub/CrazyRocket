c6e1deb051678289cddcaccfdaa4d87a05e8e1f2
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-11-19 20:22:58 +0300

    Add rocket's logic

--- core/src/main/java/hub/thespace/FirstScreen.java
+++ core/src/main/java/hub/thespace/FirstScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.ScreenUtils;
+import hub.thespace.sprites.Rocket;
 
 /**
  * First screen of the application. Displayed after the application is created.
@@ -13,14 +14,12 @@ public class FirstScreen implements Screen {
     final CrazyRocket game;
     Texture background;
 
+    Rocket rocket;
+
     public FirstScreen(CrazyRocket game) {
         this.game = game;
         background = new Texture("background.jpg");
-    }
-
-    @Override
-    public void show() {
-        // Prepare your screen here.
+        rocket = new Rocket();
     }
 
     @Override
@@ -30,8 +29,20 @@ public class FirstScreen implements Screen {
         game.batch.begin();
 
         game.batch.draw(background, 0, 0, 16, 9);
+        rocket.getSprite().draw(game.batch);
 
         game.batch.end();
+
+        logic(delta);
+    }
+
+    void logic(float delta) {
+        rocket.logic(delta);
+    }
+
+    @Override
+    public void show() {
+        // Prepare your screen here.
     }
 
     @Override
--- core/src/main/java/hub/thespace/sprites/Rocket.java
+++ core/src/main/java/hub/thespace/sprites/Rocket.java
@@ -1,14 +1,23 @@
 package hub.thespace.sprites;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class Rocket extends BasicImageSprite {
+
+    Vector2 velocity;
+    Vector2 acceleration;
 
     public Rocket() {
         super("rocket.png");
+
+        velocity = new Vector2(1, 0);
+        acceleration = new Vector2();
+
         sprite.setSize(0.5f, 5482f / 2716f / 2f);
     }
 
     @Override
     public void logic(float delta) {
-
+        sprite.translate(velocity.x * delta * 2.5f, velocity.y * delta * 2.5f);
     }
 }
