3bb2b56a99a7018feacb9ae882553a5c33356293
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-11-19 20:17:21 +0300

    Create rocket class

---
+++ core/src/main/java/hub/thespace/sprites/Rocket.java
@@ -0,0 +1,14 @@
+package hub.thespace.sprites;
+
+public class Rocket extends BasicImageSprite {
+
+    public Rocket() {
+        super("rocket.png");
+        sprite.setSize(0.5f, 5482f / 2716f / 2f);
+    }
+
+    @Override
+    void logic(float delta) {
+
+    }
+}
