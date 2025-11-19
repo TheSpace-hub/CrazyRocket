e988b674d1c1dc49a40afb39a9b78f568a78a34a
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-11-19 20:33:07 +0300

    Add a point of attraction

--- core/src/main/java/hub/thespace/sprites/Rocket.java
+++ core/src/main/java/hub/thespace/sprites/Rocket.java
@@ -1,5 +1,6 @@
 package hub.thespace.sprites;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 public class Rocket extends BasicImageSprite {
@@ -10,16 +11,23 @@ public class Rocket extends BasicImageSprite {
     public Rocket() {
         super("rocket.png");
 
-        velocity = new Vector2(1, 0);
+        velocity = new Vector2(MathUtils.random(3, 8), MathUtils.random(3, 8));
         acceleration = new Vector2();
 
+        sprite.setPosition(MathUtils.random(1, 15), MathUtils.random(1, 8));
         sprite.setSize(0.5f, 5482f / 2716f / 2f);
         sprite.setOriginCenter();
     }
 
     @Override
     public void logic(float delta) {
-        sprite.translate(velocity.x * delta * 2.5f, velocity.y * delta * 2.5f);
+        acceleration = new Vector2(
+            8f - (sprite.getX() + sprite.getWidth() / 2f),
+            4.5f - (sprite.getY() + sprite.getWidth() / 2f)
+        ).nor();
+        velocity.add(acceleration.scl(delta * 10f));
+
+        sprite.translate(velocity.x * delta, velocity.y * delta);
         sprite.setRotation(velocity.angleDeg() - 90);
     }
 }
